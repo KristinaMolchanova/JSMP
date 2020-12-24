a4df14e4308193daabf40b1a8c7f7f4cd227545b
Kristina_Molchanova <kristina_molchanova@epam.com>
2020-12-24 16:28:04 +0200

    'Node.js'

--- Frontend/src/index.ts
+++ Frontend/src/index.ts
@@ -1,7 +1,33 @@
 import './index.scss';
+const io = require('socket.io-client');
 
-function greeting(message: string) {
-  document.body.innerHTML = `<h1>${message}</h1>`;
+const socket = io.connect('http://localhost:9000', {
+  transports: ['websocket']
+});
+
+socket.on('message', (data) => {
+  console.log(data);
+  socket.emit('some event', { userName: 'Jack' });
+});
+
+function loginForm() {
+  const form = document.createElement('form');
+  const emailInput = document.createElement('input');
+  emailInput.id = 'email';
+  emailInput.type = 'text';
+  emailInput.placeholder = 'email';
+  const passwordInput = document.createElement('input');
+  passwordInput.id = 'password';
+  passwordInput.type = 'text';
+  passwordInput.placeholder = 'password';
+  const registerButton = document.createElement('button');
+  registerButton.type = 'submit';
+  registerButton.innerHTML = 'Register';
+  const loginButton = document.createElement('button');
+  loginButton.type = 'submit';
+  loginButton.innerHTML = 'Login';
+  document.body.append(form);
+  form.append(emailInput, passwordInput, registerButton, loginButton);
 }
 
-greeting('Hello World!');
+loginForm();
